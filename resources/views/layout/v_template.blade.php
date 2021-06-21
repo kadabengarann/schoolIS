40baa8cfcb338b6bfad4bd3dd2924aad75f17262
kadabengarann <rio27dh@gmail.com>
2021-06-22 07:56:24 +0800

    add favicon

--- resources/views/layout/v_template.blade.php
+++ resources/views/layout/v_template.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portal Akademik |  @yield('title')</title>
+	<link rel="icon" href="{{ asset('img/icon.ico') }}" type="image/x-icon">
 
     <link href="{{ asset('css/all.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('template/css/styles.css') }}" rel="stylesheet" />
