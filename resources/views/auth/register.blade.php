fd02c3125939ad91c90511d4c5822c5a2c824a32
kadabengarann <rio27dh@gmail.com>
2021-06-24 19:49:32 +0800

    change loi\gin.blade and register.blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
         <meta name="author" content="" />
         <title>Page Title - SB Admin</title>
         <link href="{{ asset('template')}}/css/styles.css" rel="stylesheet" />
-        <script src="{{ asset('js')}}/all.min.js" crossorigin="anonymous"></script>
+        {{-- <script src="{{ asset('js')}}/all.min.js" crossorigin="anonymous"></script> --}}
     </head>
     <body class="bg-primary">
         <div id="layoutAuthentication">
@@ -94,6 +94,6 @@
         </div>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset('template')}}/js/scripts.js"></script>
+        {{-- <script src="{{ asset('template')}}/js/scripts.js"></script> --}}
     </body>
 </html>
